ffd10bbf41a6f45b1aefe98b5fd3f402135bad5a
Timothy Prinz <tandrewprinz@nvidia.com>
2024-01-19 14:49:44 -0800

    WIP, 'no-default-features' builds

--- sh_script/pre-build.sh
+++ sh_script/pre-build.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 
-format-patch() {
+TARGET_OPTION="x86_64-unknown-none"
+process_args() {
+    while getopts ":t:" option; do
+        case "${option}" in
+            t)
+                TARGET_OPTION=${OPTARG}
+            ;;
+            *)
+                echo "Invalid option '-$OPTARG'"
+                exit 1
+            ;;
+        esac
+    done
+}
+
+patch-ring() {
     # apply the patch set for ring
     pushd external/ring
     git reset --hard 464d367252354418a2c17feb806876d4d89a8508
     git clean -xdf
-    git apply ../patches/ring/0001-Support-x86_64-unknown-none-target.patch
+    case "$TARGET_OPTION" in
+        "x86_64-unknown-none")
+            git apply ../patches/ring/0001-Support-x86_64-unknown-none-target.patch
+        ;;
+        "riscv64gc-unknown-none-elf")
+            git apply ../patches/ring/0002-Support-riscv64gc-unknown-none-elf-target.patch
+        ;;
+        *)
+            echo "Unsupported target, builds may not work!"
+        ;;
+    esac
     popd
-    
+}
+
+patch-webpki() {
     # apply the patch set for webpki
     pushd external/webpki
     git reset --hard f84a538a5cd281ba1ffc0d54bbe5824cf5969703
@@ -16,4 +43,10 @@ format-patch() {
     popd
 }
 
+format-patch() {
+    patch-ring
+    patch-webpki
+}
+
+process_args "$@"
 format-patch
